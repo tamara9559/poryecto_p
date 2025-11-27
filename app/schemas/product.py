4b6c1c9229ead6bd60e1c7f069722a03b35add01
juan támara <t4mara95@gmail.com>
2025-11-27 10:58:30 -0500

    modificaciones en la clase prodcuct

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, confloat, conint
+from pydantic import BaseModel, Field, confloat, conint, PositiveFloat, NonNegativeInt
 from typing import Optional
 from category import CategoryRead
 
@@ -6,8 +6,9 @@ from category import CategoryRead
 class ProductBase(BaseModel):
     name: str = Field(..., min_length=1, max_length=255)
     description: Optional[str] = None
-    price: confloat(ge=0) = 0.0
-    stock: conint(ge=0) = 0
+    price: PositiveFloat = 0.0
+    stock: NonNegativeInt = 0
+
 
 
 class ProductCreate(ProductBase):
@@ -17,8 +18,8 @@ class ProductCreate(ProductBase):
 class ProductUpdate(BaseModel):
     name: Optional[str] = Field(None, min_length=1, max_length=255)
     description: Optional[str] = None
-    price: Optional[confloat(ge=0)] = None
-    stock: Optional[conint(ge=0)] = None
+    price: Optional[PositiveFloat] = None
+    stock: Optional[NonNegativeInt] = None
     category_id: Optional[int] = None
 
 
