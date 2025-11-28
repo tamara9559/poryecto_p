ccbaeeba781a89260faa7ca09adf5e8288d610c4
juan támara <t4mara95@gmail.com>
2025-11-27 23:40:56 -0500

    modificacion en el archivo conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,20 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from app.database import Base
 
 TEST_DB_URL = "sqlite:///:memory:"
 
 engine = create_engine(
     TEST_DB_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False} 
 )
 
 TestingSessionLocal = sessionmaker(
-    bind=engine, autoflush=False, autocommit=False
+    bind=engine,
+    autoflush=False,
+    autocommit=False
 )
-
 
 @pytest.fixture
 def db():
@@ -23,3 +23,4 @@ def db():
     yield session
     session.close()
     Base.metadata.drop_all(bind=engine)
+
